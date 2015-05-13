6ac7773f6b3fe51ad49070b09034a4c860c9a86c
AlexUtopia <interminatus.utopia@gmail.com>
2015-05-13 09:45:38 +0300

    - дополнен скрипт tools/setup-utils.sh.

--- tools/setup-utils.sh
+++ tools/setup-utils.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 sudo apt-get install git socat
-sudo apt-get install bison flex g++ g++-multilib libgmp-dev libmpfr-dev libmpc-dev gperf libncurses5-dev libqt4-dev
+sudo apt-get install autoconf automake
+sudo apt-get install bison flex g++ g++-multilib libgmp-dev libmpfr-dev libmpc-dev gperf libncurses5-dev libqt4-dev zlib1g-dev
 sudo apt-get install avr-libc
-sudo apt-get install lpc21isp minicom
+sudo apt-get install lpc21isp libusb-1.0-0-dev minicom
+
+# Setup st-link utility
+# http://startingelectronics.org/tutorials/STM32-microcontrollers/programming-STM32-flash-in-Linux/
+# http://mainloop.ru/stm32/stm32-stlink-linux.html
+ST_LINK=tmp/st-link
+# Create build directory
+mkdir -p ${ST_LINK}/
+# Download sources
+git clone git://github.com/texane/stlink.git ${ST_LINK}/
+# Build and install tools
+cd ${ST_LINK}/; ./autogen.sh; ./configure; make; sudo make install
 
